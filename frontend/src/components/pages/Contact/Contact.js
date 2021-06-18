a25f6a305a0059bd3271b999fbfa844f1f547602
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-06-18 19:32:44 +0200

    Formulaire contact + maps

--- frontend/src/components/pages/Contact/Contact.js
+++ frontend/src/components/pages/Contact/Contact.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import '../../../App.css';
 import './Contact.css'
+import Footer from '../../Footer/Footer';
 export default function Contact() {
   return (
     <>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2521.606984559439!2d4.287394915744147!3d50.80139177952537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c67b2fd64f4d%3A0x88cb5f1a4265e337!2sKoning%20Albertstraat%2024%2C%201600%20Sint-Pieters-Leeuw!5e0!3m2!1ses!2sbe!4v1623867801078!5m2!1ses!2sbe" 
+    <section id="contact">
+      <div class="sectionheader">	<h1>CONTACT</h1></div>
+      <article>
+      <p>Besoin plus d'information ou d'un devis, n'hésitez pas à nous contacter.</p>
+        
+          <label for="checkcontact" class="contactbutton"><div class="mail"></div></label><input id="checkcontact" type="checkbox"/>
+      
+          <form action="" method="post" class="contactform">
+            <p class="input_wrapper"><input type="text" name="contact_nom" value=""  id ="contact_nom"/><label for="contact_nom">NOM</label></p>
+            <p class="input_wrapper"><input type="text" name="contact_email" value=""  id ="contact_email"/><label for="contact_email">EMAIL</label></p>
+            <p class="input_wrapper"><input type="text" name="contact_sujet" value=""  id ="contact_sujet"/><label for="contact_sujet">SUJET</label></p>
+            <p class="textarea_wrapper"><textarea name="contact_message" id="contact_message"></textarea></p>
+            <p class="submit_wrapper"><input type="submit" value="ENVOYER"/></p>			
+          </form>
+      </article>
+    </section>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2521.606984559439!2d4.287394915744147!3d50.80139177952537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c67b2fd64f4d%3A0x88cb5f1a4265e337!2sKoning%20Albertstraat%2024%2C%201600%20Sint-Pieters-Leeuw!5e0!3m2!1ses!2sbe!4v1623867801078!5m2!1ses!2sbe" 
   width="600" height="450" style={{"border":"0;"}} allowfullscreen="" loading="lazy"></iframe>
+  <Footer/>
   </>
   );
 }
