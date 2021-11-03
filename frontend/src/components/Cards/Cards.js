0c1c70d877f65a85b4ef674e1e320dbd26af33be
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2021-11-03 11:19:00 +0100

    Nouvelles cartes pour activités de l'entreprise

--- frontend/src/components/Cards/Cards.js
+++ frontend/src/components/Cards/Cards.js
@@ -7,21 +7,26 @@ function Cards() {
   return (
     <div className='cards'>
       
-      <h1>QUI SOMMES-NOUS ?</h1>
-      <div class="col-xl-6 col-lg-6 col-md pr-0">
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <div className='card'>
+      
+      <div class="left">
+      <h2>QUI SOMMES-NOUS ?</h2>
             <p class="safe-pera-one">Master Services est une entreprise spécialisée dans les travaux d’électricité générale et de l’HVAC à savoir la ventilation, la climatisation, la réfrigération, la régulation ainsi que le chauffage.</p>
             <p class="safe-pera-one">Nous assurons l’installation, la maintenance, la rénovation, et le dépannage sur tous types de chantiers : particuliers, professionnels, courant fort, courant faible.</p>
             <p class="safe-pera-one">Choisir Master Services c'est choisir la qualité. Pour nous, une implication totale est capitale. Nous fournissons des installations de qualité et nous vous prodiguons dès le début des conseils sur mesure.</p>
             <p class="safe-pera-one">Conscients de l’importance du respect des normes en vigueur, nous utilisons des matériaux agréés et de qualité, vous garantissant ainsi la qualité et la fiabilité avec laquelle vos projets méritent d’être élaborés.</p>
     
-          </div>
-           </div>
-</div></div>
-      <h1>NOS SERVICES</h1>
+      </div>
+      <div class='right'>
+      <h2> RÉNOVATION</h2>
+            <p class="safe-pera-one">Nous travaillons partout en Belgique, que ce soit dans la Region Bruxelles-Capitale, Wallonie et Flandre.</p>
+            <p class="safe-pera-one">Master Services offre toute la gamme des services de rénovation de maisons, depuis la rénovation des toilettes et de la cuisine jusqu'à la rénovation de la maison.</p>
+            <p class="safe-pera-one">Nous vous offrons la garantie d’un chantier sans soucis et de tous les avantages liés à 20 ans d’expérience dans le secteur de la peinture et de la décoration.</p>
+            <p class="safe-pera-one">Nous connaissons tous les produits et nous choisissons de travailler exclusivement avec le matériel et les fournisseurs de la meilleure qualité qui soit, afin d’effectuer un travail propre, rapide et d’un excellent rapport qualité-prix.</p>
+    
+      </div>
+      
       <div className='cards__container'>
+      <h1>NOS SERVICES</h1>
         <div className='cards__wrapper'>
           
           <ul className='cards__items'>
